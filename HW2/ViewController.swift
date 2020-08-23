8d2d68a4aeb80fec0d51532ff41125ba7f4dd3d2
Ivan Savkov <ivansavkov1387@gmail.com>
2020-08-23 23:16:17 +0300

    1. Build interface 2. Create a logic

--- HW2/ViewController.swift
+++ HW2/ViewController.swift
@@ -9,11 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        redLabel.text = String(lroundf(redSlider.value))
+        greenLabel.text = String(lroundf(greenSlider.value))
+        blueLabel.text = String(lroundf(blueSlider.value))
+        
+        
+        
     }
+    
+    @IBAction func redSliderTouch() {
+        let redRoundValue = lroundf(redSlider.value)
+        redLabel.text = String(redRoundValue)
+        mainView.backgroundColor = UIColor.init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    @IBAction func greenSliderTouch() {
+        let greenRoundValue = lroundf(greenSlider.value)
+        greenLabel.text = String(greenRoundValue)
+        mainView.backgroundColor = UIColor.init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+
+    }
+    
+    @IBAction func blueSliderTouch() {
+        let blueRoundValue = lroundf(blueSlider.value)
+        blueLabel.text = String(blueRoundValue)
+        mainView.backgroundColor = UIColor.init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+
+    }
+    
 
 
 }
