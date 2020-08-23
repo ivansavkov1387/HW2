14e1281a6f39998f75de301e012355d168f23f7c
Ivan Savkov <ivansavkov1387@gmail.com>
2020-08-23 19:47:41 +0300

    Initial Commit

---
+++ HW2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2
+//
+//  Created by Иван on 8/23/20.
+//  Copyright © 2020 Ivan Savkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
